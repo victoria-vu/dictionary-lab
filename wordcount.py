e4851b419b38b513f7ec621f90525f2fc00bc0fb
Victoria Vu <victoriamvvu@gmail.com>
2022-12-19 13:08:25 -0800

    Created dict_words function, completed function

--- wordcount.py
+++ wordcount.py
@@ -1,4 +1,29 @@
 """Count words in file."""
 
+def dict_words(filename):
 
-# put your code here.
+    data = open(filename)
+    my_dict = {}
+
+    for line in data:
+        line = line.rstrip()
+        words = line.split(" ")
+
+        for word in words:
+            if word not in my_dict:
+                my_dict[word] = 1
+            else: 
+                my_dict[word] += 1
+
+    return my_dict
+    
+print(dict_words("test.txt"))
+# print(dict_words("twain.txt"))
+
+
+#further study 
+#  for character in word:
+        #punctuation = ",.?|"
+#       if character in punctuation:
+#       #character.remove(punctuation) 
+
